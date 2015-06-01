e91841759b46cdc287b143cfa8f103bf88c85680
inkjet <rodkeyscott@gmail.com>
2015-06-01 14:43:34 -0400

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
  
 Base = declarative_base()
+
+db_location = 'sqlite:///FBS_schools.db'
  
 class School(Base):
     __tablename__ = 'school'
@@ -16,11 +18,14 @@ class School(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), default="Not Set", nullable=False)
     isPowerFive = Column(Integer, default=1, nullable=False)
+
+def create_db():
  
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
-engine = create_engine('sqlite:///FBS_schools.db')
+    # Create an engine that stores data in the local directory's
+    # sqlalchemy_example.db file.
+    engine = create_engine(db_location)
  
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-Base.metadata.create_all(engine)
+    # Create all tables in the engine. This is equivalent to "Create Table"
+    # statements in raw SQL.
+    Base.metadata.create_all(engine)
+    print('School database created')
